a12e21bb36695750e1c2ce53e2802fd1b30c134a
NassarX <mahmoud@nassarx.com>
2023-03-09 15:29:09 +0000

    Add `Task` model class

---
+++ app/src/api/models/Task.ts
@@ -0,0 +1,62 @@
+import { Model, Table, Column, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import User from "./User";
+
+@Table({
+  timestamps: true,
+  tableName: 'tasks',
+  paranoid: true //imposes a soft delete on the model
+})
+class Task extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id!: number;
+
+  @Column({
+    type: DataType.STRING(250),
+    allowNull: false
+  })
+  title!: string;
+
+  @Column({
+    type: DataType.STRING(2500),
+    allowNull: false
+  })
+  summary!: string;
+
+  @Column({
+    type: DataType.DATEONLY,
+    defaultValue: DataType.NOW,
+  })
+  performedAt!: Date;
+
+  @ForeignKey(() => User)
+  @Column({
+    allowNull: false
+  })
+  userId!: number;
+
+  @BelongsTo(() => User)
+  user!: User;
+
+  @Column({
+    type: DataType.DATE,
+    defaultValue: DataType.NOW,
+  })
+  createdAt?: Date;
+
+  @Column({
+    type: DataType.DATE,
+    defaultValue: DataType.NOW,
+    onUpdate: 'NOW()'
+  })
+  updatedAt?: Date;
+
+  @Column({
+    type: DataType.DATE
+  })
+  deletedAt?: Date;
+}
+export default Task;

7ff5c2fb75e8b17bf6093ccb3a0b0edb9a65952a
NassarX <mahmoud@nassarx.com>
2023-03-09 15:11:38 +0000

    Install `Zod` package for schema validations, Add missing packages to package.json

---
+++ app/src/api/middlewares/validate.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import * as z from 'zod';
+import { BadRequestError } from '../../utils/ApiError';
+import Logger from '../../utils/Logger';
+
+
+export const validate =
+  (schema: z.AnyZodObject | z.ZodOptional<z.AnyZodObject>) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync(req);
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const message = error.issues.map((issue) => issue.message).join(', ');
+        Logger.error(message);
+        next(new BadRequestError(message, error.issues.map((issue) => issue.message)));
+      } else {
+        next(error);
+      }
+    }
+  };

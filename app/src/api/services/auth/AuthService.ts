b39702fae87e6a6c5e1ae996a1f05835b3d83893
NassarX <mahmoud@nassarx.com>
2023-03-09 15:21:00 +0000

    Add `TokenService` to handle tokens related operations generate, refresh, etc ..

---
+++ app/src/api/services/auth/AuthService.ts
@@ -0,0 +1,51 @@
+import { Inject, Service } from 'typedi';
+import TokenService from "./TokenService";
+import {LoginUserDto, UserDto} from "../../types/user.dto";
+import {UserRepository} from "../../repositories/UserRepository";
+import {UserHydrator} from "../../../utils/Helpers";
+
+@Service()
+/**
+ * User Service
+ */
+export default class AuthService {
+  userRepository: UserRepository;
+  tokenService: TokenService;
+  userHydrator: UserHydrator
+
+  constructor(@Inject() userRepository: UserRepository, @Inject() tokenService: TokenService, @Inject() userHydrator: UserHydrator) {
+    this.userRepository = userRepository;
+    this.tokenService = tokenService;
+    this.userHydrator = userHydrator;
+  }
+
+  authUser = async (user: LoginUserDto) => {
+    const userInstance = await this.userRepository.getUserByUserName(user.username);
+    await this.userRepository.isPasswordMatched(userInstance, user.password)
+
+    const userPermissions = await userInstance.role.getPermissions();
+    const userDto = this.userHydrator.hydrate(userInstance.get(), userPermissions);
+
+    console.log(userDto);
+
+    const tokens = await this.tokenService.generateAuthTokens(userDto);
+    return {
+      user: userDto,
+      tokens: tokens
+    }
+  }
+
+
+
+  /**
+   * Refresh auth tokens
+   *
+   * @param refreshToken
+   **/
+  refreshAuth = (refreshToken: string) => {
+    //@TODO
+  }
+
+
+
+}

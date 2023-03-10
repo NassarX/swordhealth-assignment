162c33f57efd68491cd8340e53767a53ed299ac7
NassarX <mahmoud@nassarx.com>
2023-03-10 13:10:51 +0000

    Reconfigure passport jwt strategy setting user type to UserDto interface

--- app/src/config/passport.ts
+++ app/src/config/passport.ts
@@ -1,11 +1,11 @@
 import passportJwt from 'passport-jwt';
 import Env from "../config/app.config";
 import {Container} from "typedi";
-import {UserRepository} from "../api/repositories/UserRepository";
+import UserService from "../api/services/UserService";
 
 const JwtStrategy = passportJwt.Strategy
 const ExtractJwt = passportJwt.ExtractJwt
-const userRepository = Container.get(UserRepository);
+const userService = Container.get(UserService);
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,7 +13,7 @@ const jwtOptions = {
 };
 
 const jwtStrategy = new JwtStrategy(jwtOptions, async (jwt_payload: any, done) => {
-  await userRepository.getUserByUserName(jwt_payload.sub.toLowerCase())
+  await userService.getUserByUserName(jwt_payload.sub.toLowerCase())
     .then((user) => { return done(null, user); })
     .catch((error) => { return done(error, false); });
 });
@@ -22,7 +22,7 @@ export default jwtStrategy;
 
 /**
  * passport-local The local authentication strategy authenticates users using a username and password.
- * The strategy requires a verify callback, which accepts these credentials and calls done providing a user.
+ * The strategy requires verify callback, which accepts these credentials and calls done providing a user.
  *
  * passport-jwt This module lets you authenticate endpoints using a JSON web token.
  * It is intended to be used to secure RESTful endpoints without sessions.
